934e8b40d3b245a1c9cb4affc15856643da12897
shurikev <shurikev@gmail.com>
2017-09-25 16:33:50 +0700

    Промежуточный Dockerfile для установки КриптоПро. Требуется перезагрузка

---
+++ CryptoPro.Net/Start.ps1
@@ -0,0 +1,3 @@
+# TODO - use ServiceMonitor.exe when it gets open-sourced (https://github.com/Microsoft/iis-docker/issues/1)
+Write-Verbose "Started CryptoPro."
+while ($true) { Start-Sleep -Seconds 3600 }
